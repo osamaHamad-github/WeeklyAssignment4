5b56602a8ffb0308a9b9655a11ee60aac6f51ec9
osamaHamad0 <>
2021-05-03 13:49:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// function to go to linus' github
+function githubButton () {
+  window.location.assign('https://github.com/torvalds')
+};
+
+// function to go to linus' wiki
+function wikiButton () {
+  window.location.assign('https://en.wikipedia.org/wiki/Linus_Torvalds')
+};
+
+// function to go to linus' twitter
+function twitterButton () {
+  window.location.assign('https://twitter.com/linus__torvalds?lang=en')
+};
